d8e0e674a94f9488b6c61fc148a248677625be23
Terrence Mahnken <terrencemm2@gmail.com>
2024-03-02 18:45:28 -0600

    config: change and set prompt color

--- ConsoleApp/src/Prompt/Prompt.cs
+++ ConsoleApp/src/Prompt/Prompt.cs
@@ -1,11 +1,12 @@
 public class Prompt
 {
-  public static ConsoleColor foregroundColor = ConsoleColor.Blue;
+  public static ConsoleColor foregroundColor = ConsoleColor.Green;
   public static string mainMenuTitle = "Zelda Database";
   public static string consoleLabel = "Zelda Database";
   public Prompt()
   {
     Console.Title = consoleLabel;
+    Console.ForegroundColor = foregroundColor;
   }
 
   public void ShowMenu()
